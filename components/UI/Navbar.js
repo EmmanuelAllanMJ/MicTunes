4b7a0725a087fd659d0bff36d7106b9cb3513ac5
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-05-18 13:53:37 +0530

    Added prisma schema

--- components/UI/Navbar.js
+++ components/UI/Navbar.js
@@ -199,7 +199,7 @@ function Navbar() {
             </li>
             <li>
               <a
-                href="#"
+                href="/user/"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <svg
